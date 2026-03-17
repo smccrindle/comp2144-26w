af7f0433ea8f3a36e93c0c4bccbcc8a3566a5ae7
Scott McCrindle <iainmccrindle@gmail.com>
2026-03-17 17:04:00 -0400

    Completed all three lesson files for Sphero Bolt+ both in JS and proprietary .lab format

--- lesson-12/sphero-12a.js
+++ lesson-12/sphero-12a.js
@@ -1,27 +1,31 @@
-// STEP 1: Create the main program asynchronous function
+//  STEP 1: Create the main program asynchronous function;
+async function startProgram() {
+    //  STEP 2: Instruct the main LED light with rgb (using object notation);
+    setMainLed({ "r": 0, "g": 255, "b": 150 });
+    //  STEP 3: Text-to-speech;
+    await speak("Starting the lesson program sequence.", false);
+    //  STEP 4: Wait for a short period of time;
+    await delay(1);
+    //  STEP 5: Construct a loop that will iterate 4 times;
+    for (let i = 0; i < 4; i++) {
+        //  STEP 6: Set the main LED light to a random color;
+        setMainLed(getRandomColor());
+        //  STEP 7: Play a fun random sound;
+        //  Note: Bolt+ supports a wide variety of library sounds;
+        await Sound.Game.Coin.play(true);
+        //  STEP 8: Use the LCD display screen;
+        //  This shows the current loop iteration on the Bolt+ screen;
+        await setDisplayText("Go!", { "r": 0, "g": 0, "b": 0 }, { "r": 255, "g": 255, "b": 255 }, 1);
+        //  STEP 9: Turn 90 degrees, then roll at speed 0-255, for 1 second;
+        //  We multiply 90 by i to make it turn in a square pattern;
+        await roll(90 * i, 100, 1);
+        //  STEP 10: Announce the current navigational heading in degrees;
+        await speak("Heading is " + ("" + getHeading()) + " degrees", true);
+        //  STEP 11: Take a one second break;
+        await delay(1);
+    }
 
-    // STEP 2: Instruct the main LED light with rgb (using object notation)
-
-    // STEP 3: Text-to-speech
-
-    // STEP 4: Wait for a short period of time
-
-    // STEP 5: Construct a loop that will iterate 4 times
-
-        // STEP 6: Set the main LED light to a random color
-
-        // STEP 7: Play a fun random sound
-        // Note: Bolt+ supports a wide variety of library sounds
-
-        // STEP 8: Use the LCD display screen
-        // This shows the current loop iteration on the Bolt+ screen
-
-        // STEP 9: Turn 90 degrees, then roll at speed 0-255, for 1 second
-        // We multiply 90 by i to make it turn in a square pattern
-
-        // STEP 10: Announce the current navigational heading in degrees
-
-        // STEP 11: Take a one second break
-
-    // STEP 12: Say how far the robot has travelled (in cm)
+    //  STEP 12: Say how far the robot has travelled (in cm);
+    await speak("Total distance travelled is " + ("" + getDistance()) + " centimeters", true);
+}
 
--- lesson-12/sphero-12b.js
+++ lesson-12/sphero-12b.js
@@ -1,11 +1,25 @@
 // STEP 1: Create an event handler to listen for collisions, and add an anonymous function to handle the event
-
+registerEvent(EventType.onCollision, async () => {
     // STEP 2: Stop the robot from rolling, set the main LED to red, and play an explosion animation
-    
+    stopRoll();
+    setMainLed({
+        r: 255,
+        g: 0,
+        b: 0
+    });
+    await setDisplayImage(DisplayImage.Explosion);
     // STEP 3a: Make the robot say, "Collision", create a variable for a random heading, then set the heading
-    
+    await speak("I crashed!", true);
+    let newHeading = getHeading() + 180;
+    await spin(newHeading, 0.5);
     // STEP 3b: Wait for half a second, set the main LED to white, and roll again
-    
-
+    await delay(0.5);
+    setMainLed(getRandomColor());
+    setSpeed(255);
+});
 // STEP 4: Run the program and see how the robot reacts to collisions
-
+async function startProgram() {
+    setMainLed({
+        
+    })
+}
